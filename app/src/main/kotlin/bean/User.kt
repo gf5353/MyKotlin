03bf26cc64b5afe730a1b1efbf237a529e687580
gufei <gufei@plu.cn>
2016-03-15 19:44:34 +0800

    commit sample1

--- app/src/main/kotlin/bean/User.kt
+++ app/src/main/kotlin/bean/User.kt
@@ -2,8 +2,19 @@ package bean
 
 /**
  * Created by gf535 on 2016/3/13 0013.
+ * var 可变参数类型，val不可变参数类型相当于final
+ *
  */
 data class User(
-        var name: String,
-        var age: Int
+        var name: String ,
+        var age: Int,
+        var email: String
+
+        //private -- 只在声明的范围和同一个模块的子范围可见；
+        //
+        //protected -- (只可以用在类或接口的成员上)和 privete 很像，但在子类中也可见；
+        //
+        //internal -- (默认使用) 在同一个模块中都可见；
+        //
+        //public -- 在任何地方均可见；
 )
--- app/src/main/kotlin/example/SampleActivity1.kt
+++ app/src/main/kotlin/example/SampleActivity1.kt
@@ -3,32 +3,22 @@ package example
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Toast
 import bean.User
 import com.example.mykotlin.R
 import kotlinx.android.synthetic.main.activity_sample1.*
 
 /**
  * Created by gf535 on 2016/3/13 0013.
+ *
+ * 数据对象的使用
  */
 class SampleActivity1 : AppCompatActivity() {
 
-    /*
-     * 可变参数申明类型
+
+    /**
+     *1.函数方法的订阅
+     * 参数：类型 返回类型也基本和java语法相反
      */
-    var x: Int = 5
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample1)
-
-        val user = User("小明", 2)//name,age,对应构造函数类型 var是可变变量，val是不可变(相当于final)
-        var usr = User("小红", 3)
-
-        x += 1
-        Toast.makeText(baseContext, "总年龄：" + sum(user.age, usr.age), Toast.LENGTH_LONG).show();
-    }
-
     fun sum(a: Int, b: Int): Int {
         var sum = a + b
         print(sum)
@@ -36,7 +26,42 @@ class SampleActivity1 : AppCompatActivity() {
     }
 
     fun sum2(a: Int, b: Int): Int = a + b
-
     fun sum3(a: Int, b: Int) = a + b
+
+    /*
+       * 可变参数申明类型
+          */
+    private var x: Int = 5
+    private var tag: String = "SampleActivity1"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample1)
+        createMode()
+        changeData()
+    }
+
+    /**
+     * 2.对象创建，及相关方法说明
+     */
+    fun createMode() {
+        val user = User("小明", 2, "562401002@qq.com")//name,age,对应构造函数类型 var是可变变量，val是不可变(相当于final)
+        var usr = user.copy(name = "小红")//"小红", 3
+        Log.d(tag, String.format("name = %s email = %s", usr.component1(), usr.component3()))//get方法按照构造函数的顺序生成，不过貌似没有set方法
+        //字符串模板表达式
+        var name1 = user.name
+        var name2 = usr.name
+        var sum = "$name1 和 $name2 的年龄之和为 ${sum(user.age, usr.age)}"
+        Log.d(tag, sum)
+    }
+
+    /**
+     *3.基本类型转换
+     */
+    fun changeData() {
+        val b: Byte = 1
+        val i: Int = b.toInt() //显式转换
+    }
 
 }
